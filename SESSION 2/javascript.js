840520b351a3a7c5a9aef9cd1dbd7d746fd1025f
iaw39434895 <iaw39434895@h28.informatica.escoladeltreball.org>
2021-03-10 20:07:15 +0100

    form

--- SESSION 2/javascript.js
+++ SESSION 2/javascript.js
@@ -1,2 +1,12 @@
 var nifRegex = /^[0-9]{8}[TRWAGMYFPDXBNJZSQVHLCKE]$/i;
 var nieRegex = /^[XYZ][0-9]{7}[TRWAGMYFPDXBNJZSQVHLCKE]$/i;
+var mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+function validate(){
+    validardocumento();
+    validarcontrasenya();
+
+
+
+
+
+};
