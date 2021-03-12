a068786e85e1d01723bee45a9b3581bc1aeedc07
iaw39434895 <iaw39434895@h21.informatica.escoladeltreball.org>
2021-03-12 18:43:57 +0100

    update

--- SESSION 2/javascript.js
+++ SESSION 2/javascript.js
@@ -4,9 +4,14 @@ var mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9
 function validate(){
     validardocumento();
     validarcontrasenya();
-
-
-
-
-
 };
+function rellenar(){
+    console.log("Llegó");
+    var div = document.getElementById("inputdocument");
+    var pasaporte = document.createElement("input");
+    pasaporte.setAttribute("type", "radio");
+    pasaporte.innerText="Pasaporte";
+    pasaporte.setAttribute("id","pasaporte");
+    pasaporte.setAttribute("name","contact");
+    div.appendChild(pasaporte);
+}
